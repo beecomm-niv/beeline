e8347c8641e9bc56767e951a05fb75647ab0e3d0
beecommrnd <beecommrnd@gmail.com>
2025-10-19 15:23:47 +0300

    .

--- app/components/otp/index.tsx
+++ app/components/otp/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Button, styled, TextField, Typography } from '@mui/material';
+import { Box, Button, Card, Typography } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 
 import LockIcon from '@mui/icons-material/Lock';
@@ -15,36 +15,19 @@ interface Props {
 }
 
 export const OTP = (props: Props) => {
-  const [code, setCode] = useState<string[]>(['', '', '', '']);
+  const [code, setCode] = useState<string>('');
   const [timer, setTimer] = useState(TIMEOUT_LENGTH);
 
-  const ref1 = useRef<HTMLInputElement | null>(null);
-  const ref2 = useRef<HTMLInputElement | null>(null);
-  const ref3 = useRef<HTMLInputElement | null>(null);
-  const ref4 = useRef<HTMLInputElement | null>(null);
-
   const timeout = useRef<NodeJS.Timeout | undefined>(undefined);
+  const ref = useRef<HTMLInputElement | null>(null);
 
-  const onChange = (key: string, index: number, currentRef: HTMLInputElement | null, nextRef: HTMLInputElement | null, prevRef: HTMLInputElement | null) => {
-    const val = key === 'Backspace' ? '' : key;
+  const onChange = (code: string) => {
+    if (!isNaN(+code) && code.length <= 4) {
+      setCode(code);
 
-    if (val && isNaN(Number(val))) {
-      return;
-    }
-
-    setCode((c) => {
-      const clone = [...c];
-      clone[index] = val;
-      return clone;
-    });
-
-    if (val) {
-      nextRef?.focus();
-      if (!nextRef) {
-        currentRef?.blur();
+      if (code.length === 4) {
+        ref.current?.blur();
       }
-    } else if (!code[index]) {
-      prevRef?.focus();
     }
   };
 
@@ -77,28 +60,9 @@ export const OTP = (props: Props) => {
 
   const onSubmit = () => {
     if (!props.isLoading) {
-      props.onSubmit(code.join(''));
+      props.onSubmit(code);
     }
   };
-
-  useEffect(() => {
-    if ('OTPCredential' in window) {
-      const ac = new AbortController();
-      navigator.credentials
-        .get({
-          otp: { transport: ['sms'] },
-          signal: ac.signal,
-        } as any)
-        .then((otp: any) => {
-          window.alert(otp?.code);
-          ac.abort();
-        })
-        .catch((err) => {
-          ac.abort();
-          window.alert(err);
-        });
-    }
-  }, []);
 
   return (
     <Box sx={{ display: 'flex', height: '100svh', width: '100%', alignItems: 'center', flexDirection: 'column', padding: '20% 5% 0 5%', gap: 3 }}>
@@ -108,12 +72,22 @@ export const OTP = (props: Props) => {
       </Typography>
 
       <Box>
-        <Box sx={{ display: 'flex', gap: 2, direction: 'ltr' }}>
-          <Input inputRef={ref1} value={code[0]} onKeyDown={(e) => onChange(e.key, 0, ref1.current, ref2.current, null)} type='number' inputMode='numeric' autoComplete='one-time-code' />
-          <Input inputRef={ref2} value={code[1]} onKeyDown={(e) => onChange(e.key, 1, ref2.current, ref3.current, ref1.current)} type='number' />
-          <Input inputRef={ref3} value={code[2]} onKeyDown={(e) => onChange(e.key, 2, ref3.current, ref4.current, ref2.current)} type='number' />
-          <Input inputRef={ref4} value={code[3]} onKeyDown={(e) => onChange(e.key, 3, ref4.current, null, ref3.current)} type='number' />
+        <Box sx={{ display: 'flex', gap: 2, direction: 'ltr' }} onClick={() => ref.current?.focus()}>
+          {[1, 2, 3, 4].map((c) => (
+            <Card key={c} sx={{ padding: 4, height: 30, width: 40, display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10%' }}>
+              <Typography fontSize={25}>{code.at(c - 1) || ''}</Typography>
+            </Card>
+          ))}
         </Box>
+
+        <input
+          style={{ position: 'absolute', top: 0, left: 0, width: 0, height: 0, opacity: 0 }}
+          ref={ref}
+          type='text'
+          inputMode='numeric'
+          autoComplete='one-time-code'
+          onChange={(e) => onChange(e.target.value)}
+        />
 
         <Button variant='contained' fullWidth sx={{ borderRadius: '10px', marginTop: 4 }} onClick={onSubmit} disabled={props.isLoading}>
           שליחה
@@ -129,15 +103,3 @@ export const OTP = (props: Props) => {
     </Box>
   );
 };
-
-const Input = styled(TextField)({
-  '& .MuiInputBase-root': {
-    paddingTop: 0,
-  },
-  '& .MuiInputBase-input': {
-    height: 30,
-    width: 40,
-    textAlign: 'center',
-    fontSize: 25,
-  },
-});
